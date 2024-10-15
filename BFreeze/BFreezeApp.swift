4b018105c3e6be43767df12c9b061f58b09fca69
Josephine Lo <jlo2@scu.edu>
2024-10-14 23:08:03 -0700

    Initial Commit

---
+++ BFreeze/BFreezeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BFreezeApp.swift
+//  BFreeze
+//
+//  Created by jlo on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct BFreezeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BFreezeTests/BFreezeTests.swift
@@ -0,0 +1,16 @@
+//
+//  BFreezeTests.swift
+//  BFreezeTests
+//
+//  Created by jlo on 10/14/24.
+//
+
+import Testing
+
+struct BFreezeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
